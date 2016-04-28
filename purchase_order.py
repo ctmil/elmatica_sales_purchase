367a94d883a0c1b117a3467add56ebb355f4d762
morrillo <gustavo.orrillo@moldeo.coop>
2016-04-28 19:15:45 -0300

    Added related TPO/PPO

--- purchase_order.py
+++ purchase_order.py
@@ -10,6 +10,24 @@ class purchase_order(models.Model):
 	_inherit = 'purchase.order'
 
 	@api.one
+	def _compute_related_ppo(self):
+		pos = self.sale_order_id.purchase_order_ids
+		return_value = None
+		for po in pos:
+			if po.order_type == 'PPO':
+				return_value = po.id
+		self.related_ppo = return_value			
+
+	@api.one
+	def _compute_related_tpo(self):
+		pos = self.sale_order_id.purchase_order_ids
+		return_value = None
+		for po in pos:
+			if po.order_type == 'TPO':
+				return_value = po.id
+		self.related_tpo = return_value			
+
+	@api.one
 	def _calc_hub_days20(self):
 		#supplier = self.partner_id
 		#import pdb;pdb.set_trace()
@@ -24,3 +42,5 @@ class purchase_order(models.Model):
 
 	sale_order_id = fields.Many2one('sale.order',string='Origin SO')
 	hub_days = fields.Integer(string='Autoline days',compute=_calc_hub_days20)
+	related_ppo = fields.Many2one('purchase.order',string='Related PPO',compute=_compute_related_ppo)
+	related_tpo = fields.Many2one('purchase.order',string='Related TPO',compute=_compute_related_ppo)
