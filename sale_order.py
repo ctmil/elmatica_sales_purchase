62bac3efbf4ffee81ace85be3cfbe2a347a90c7e
morrillo <gustavo.orrillo@moldeo.coop>
2016-05-31 20:43:35 -0300

    Removed pdb

--- sale_order.py
+++ sale_order.py
@@ -80,7 +80,6 @@ class sale_order(models.Model):
 					line_product = line_pack.product_name.id
 					partner_id = line_pack.product_name.supplier_id.id
        	        			# cost_unit = line.unit_cost
-					import  pdb;pdb.set_trace()
 					pricelist = sale.selected_supplier.property_product_pricelist_purchase
 					return_pricelist = pricelist.price_get(line_product, line.product_uom_qty or 1.0, partner_id or False,\
 						 context = {'uom': line.product_uom.id, 'date': str(date.today())})
