d3095ac563b3d417ff36db814596a9a2547d6145
morrillo <gustavo.orrillo@moldeo.coop>
2016-06-15 19:41:35 -0300

    Fixed bug

--- sale_order.py
+++ sale_order.py
@@ -91,7 +91,7 @@ class sale_order(models.Model):
 			# Checks MOV
 			if line_product:
 				suppinfo = self.env['product.supplierinfo'].search([('partner_id','=',sale.selected_supplier.id),\
-							('product_tmpl_id','=',line_product.product_tmpl_id.id)])
+							('product_tmpl_id','=',line.product_id.product_tmpl_id.id)])
 				if suppinfo:
 					if suppinfo.mov:
 						if (cost_unit * line.product_uom_qty) < suppinfo.mov:
