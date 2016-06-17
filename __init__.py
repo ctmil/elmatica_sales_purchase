bfb118bb06a494bcfaefab0ad1068c2904c93c25
morrillo <gustavo.orrillo@moldeo.coop>
2016-06-17 06:54:48 -0300

    Added return view to make order function

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ import models
 import sale_order
 import purchase_order
 import purchase_old
+import custom_crm
---
+++ custom_crm.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+from openerp.osv import osv, fields
+from datetime import datetime
+from dateutil import parser
+from datetime import date
+
+	
+class crm_make_sale(osv.osv_memory):
+    """ Make sale  order for crm """
+
+    _inherit = "crm.make.sale"
+    _description = "Make sales"
+
+    def makeOrder(self, cr, uid, ids, context=None):
+	"""
+        This function  create Quotation on given case.
+        @param self: The object pointer
+        @param cr: the current row, from the database cursor,
+        @param uid: the current user’s ID for security checks,
+        @param ids: List of crm make sales' ids
+        @param context: A standard dictionary for contextual values
+        @return: Dictionary value of created sales order.
+        """
+        # update context: if come from phonecall, default state values can make the quote crash lp:1017353
+        context = dict(context or {})
+        context.pop('default_state', False)        
+        
+        case_obj = self.pool.get('crm.lead')
+        sale_obj = self.pool.get('sale.order')
+	sale_line_obj = self.pool.get('sale.order.line')
+        partner_obj = self.pool.get('res.partner')
+        data = context and context.get('active_ids', []) or []
+
+        for make in self.browse(cr, uid, ids, context=context):
+            partner = make.partner_id
+            partner_addr = partner_obj.address_get(cr, uid, [partner.id],
+                    ['default', 'invoice', 'delivery', 'contact'])
+            pricelist = partner.property_product_pricelist.id
+            fpos = partner.property_account_position and partner.property_account_position.id or False
+            payment_term = partner.property_payment_term and partner.property_payment_term.id or False
+            new_ids = []
+            for case in case_obj.browse(cr, uid, data, context=context):
+		product_name = case.product_name.id 		
+		if not partner and case.partner_id:
+                    partner = case.partner_id
+                    fpos = partner.property_account_position and partner.property_account_position.id or False
+                    payment_term = partner.property_payment_term and partner.property_payment_term.id or False
+                    partner_addr = partner_obj.address_get(cr, uid, [partner.id],
+                            ['default', 'invoice', 'delivery', 'contact'])
+                    pricelist = partner.property_product_pricelist.id
+                if not case.requested_date:
+		    requested_date = str(date.today())
+		else:
+		    requested_date = case.requested_date
+                    # raise osv.except_osv(_('Insufficient Data!'), _('No requested_date for this opportunity.'))
+                if False in partner_addr.values():
+                    raise osv.except_osv(_('Insufficient Data!'), _('No address(es) defined for this customer.'))
+
+                vals = {
+                    'origin': ('Opportunity: %s') % str(case.id),
+                    'section_id': case.section_id and case.section_id.id or False,
+                    'categ_ids': [(6, 0, [categ_id.id for categ_id in case.categ_ids])],
+                    'partner_id': partner.id,
+		    'opportunity_id': case.id,
+                    'pricelist_id': pricelist,
+                    'partner_invoice_id': partner_addr['invoice'],
+                    'partner_shipping_id': partner_addr['delivery'],
+                    'date_order': fields.datetime.now(),
+                    'fiscal_position': fpos,
+                    'payment_term':payment_term,
+		    'requested_date': requested_date,
+		    'requested_delivery_date': requested_date,
+		    'customer_project': case.customer_project,
+                    'note': sale_obj.get_salenote(cr, uid, [case.id], partner.id, context=context),
+                }
+                if partner.id:
+                    vals['user_id'] = partner.user_id and partner.user_id.id or uid
+                if partner.sale_warn_msg:
+                    vals['custom_message'] = partner.sale_warn_msg
+                new_id = sale_obj.create(cr, uid, vals, context=context)
+                sale_order = sale_obj.browse(cr, uid, new_id, context=context)
+		sale_line_obj.create(cr,uid,{'order_id':new_id,'product_id':product_name},context=None)
+                case_obj.write(cr, uid, [case.id], {'ref': 'sale.order,%s' % new_id})
+	        # result =  super(sale_order, self).onchange_partner_id(cr, uid, [sale_order.id], partner.id, context=context)
+	        result = sale_order.onchange_partner_id(partner.id, context=context)
+
+                new_ids.append(new_id)
+                message = ("Opportunity has been <b>converted</b> to the quotation <em>%s</em>.") % (sale_order.name)
+                case.message_post(body=message)
+            if make.close:
+                case_obj.case_mark_won(cr, uid, data, context=context)
+            if not new_ids:
+                return {'type': 'ir.actions.act_window_close'}
+            if len(new_ids)<=1:
+                value = {
+                    'domain': str([('id', 'in', new_ids)]),
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'res_model': 'sale.order',
+                    'view_id': False,
+                    'type': 'ir.actions.act_window',
+                    'name' : ('Quotation'),
+                    'res_id': new_ids and new_ids[0]
+                }
+            else:
+                value = {
+                    'domain': str([('id', 'in', new_ids)]),
+                    'view_type': 'form',
+                    'view_mode': 'tree,form',
+                    'res_model': 'sale.order',
+                    'view_id': 'elmatica_sales_purchase.view_order_form_sales_purchase_custom',
+                    'type': 'ir.actions.act_window',
+                    'name' : ('Quotation'),
+                    'res_id': new_ids
+                }
+            return value
+	
+class crm_lead2opportunity_partner(osv.osv_memory):
+    _inherit = 'crm.lead2opportunity.partner'
+
+    _columns = {
+	'qty': fields.integer('Quantity',default=1,required=True)
+	}
+
+    def action_apply(self, cr, uid, ids, context=None):
+        """
+        Convert lead to opportunity or merge lead and opportunity and open
+        the freshly created opportunity view.
+        """
+        if context is None:
+            context = {}
+
+        lead_obj = self.pool['crm.lead']
+        partner_obj = self.pool['res.partner']
+
+        w = self.browse(cr, uid, ids, context=context)[0]
+        opp_ids = [o.id for o in w.opportunity_ids]
+        vals = {
+            'section_id': w.section_id.id,
+        }
+        if w.partner_id:
+            vals['partner_id'] = w.partner_id.id
+        if w.name == 'merge':
+            lead_id = lead_obj.merge_opportunity(cr, uid, opp_ids, context=context)
+            lead_ids = [lead_id]
+            lead = lead_obj.read(cr, uid, lead_id, ['type', 'user_id'], context=context)
+            if lead['type'] == "lead":
+                context = dict(context, active_ids=lead_ids)
+                vals.update({'lead_ids': lead_ids, 'user_ids': [w.user_id.id]})
+                self._convert_opportunity(cr, uid, ids, vals, context=context)
+            elif not context.get('no_force_assignation') or not lead['user_id']:
+                vals.update({'user_id': w.user_id.id})
+                lead_obj.write(cr, uid, lead_id, vals, context=context)
+        else:
+	    n_qty = w.qty
+	    if n_qty < 1:
+		n_qty = 1
+            lead_ids = context.get('active_ids', [])
+	    temp_lead_id = lead_ids[0]
+	    temp_lead_ids = [temp_lead_id]
+	    for index in range(n_qty - 1):
+		    name = ''
+		    for lead in self.pool.get('crm.lead').browse(cr,uid,lead_ids):
+			name = lead.name
+		    # name = name + ' - ' + str(index+1) + ' of ' + str(n_qty - 1)
+		    name = name + ' - ' + str(index+2) + ' of ' + str(n_qty)
+		    lead_id = self.pool.get('crm.lead').copy(cr,uid,temp_lead_id)
+		    # This is where we copy attachments
+		    attachment_ids = self.pool.get('ir.attachment').search(cr,uid,[('res_id','=',temp_lead_id),('res_model','=','crm.lead')])
+		    for attachment_id in attachment_ids:
+			attach_id = self.pool.get('ir.attachment').copy(cr,uid,attachment_id)
+			vals_attachment = {
+				'res_id': lead_id,
+				}
+			return_id = self.pool.get('ir.attachment').write(cr,uid,[attach_id],vals_attachment)
+		    return_id = self.pool.get('crm.lead').write(cr,uid,[lead_id],{'name':name})
+		    temp_lead_ids.append(lead_id)
+       	    vals.update({'lead_ids': temp_lead_ids, 'user_ids': [w.user_id.id]})
+            self._convert_opportunity(cr, uid, ids, vals, context=context)
+            # self._convert_opportunity(cr, uid, temp_lead_ids, vals, context=context)
+       	    for lead in lead_obj.browse(cr, uid, lead_ids, context=context):
+                if lead.partner_id and lead.partner_id.user_id != lead.user_id:
+       	            partner_obj.write(cr, uid, [lead.partner_id.id], {'user_id': lead.user_id.id}, context=context)
+
+        return self.pool.get('crm.lead').redirect_opportunity_view(cr, uid, lead_ids[0], context=context)
+
