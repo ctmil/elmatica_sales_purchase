8c9dfb6a81c0f4304c6be4fd579e15bd0d9e02f2
morrillo <gustavo.orrillo@moldeo.coop>
2016-07-01 11:16:17 -0300

    First shot at new delivered_qty

--- sale_order_line.py
+++ sale_order_line.py
@@ -13,12 +13,23 @@ class sale_order_line(models.Model):
 	@api.one
 	def _calc_delivered_qty_v2(self):
 		return_value = 0
-		:wq
+		if self.product_id.is_pack:
+			for pack_line in self.product_id.wk_product_pack:
+				if pack_line.product_name.product_tmpl_id.ntty_id and pack_line.product_name.product_tmpl_id.ntty_id != '':
+					pcb_product = pack_line.product_name.id
+			pickings = self.env['stock.picking'].search([('origin','=',self.order_id.name)])
+			for picking in pickings:
+				if picking.state == 'done':
+					for move_line in picking.move_lines:
+						if move_line.product_id.id == pcb_product:
+							return_value = return_value + move_line.product_uom_qty
+		self.delivered_qty = return_value	
 
 
 	@api.one
 	def _calc_undelivered_qty_v2(self):
 		return_value = 0
+		self.undelivered_qty = return_value	
 
 
 	delivered_qty = fields.Float('Delivered', compute='_calc_delivered_qty_v2')
