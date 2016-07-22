e02180469bf0a597998e9cee052212967b8594a8
morrillo <gustavo.orrillo@moldeo.coop>
2016-07-22 09:16:50 -0300

    Modified __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -2,7 +2,7 @@
     'name': 'Elmatica - Sales/Purchase',
     'category': 'Sales',
     'version': '0.2',
-    'depends': ['elmatica_sales_customizations','elmatica_invoice','elmatica_purchase_flow','sale_crm','email_template'],
+    'depends': ['elmatica_sales_customizations','elmatica_invoice','elmatica_purchase_flow','sale_crm','email_template','elmatica_wms'],
     'data': [
 	'sale_view.xml',
     ],
