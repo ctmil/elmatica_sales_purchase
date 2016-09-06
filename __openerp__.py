abc768b7b3aa1a3519b4602da608a4bfff6e9769
morrillo <gustavo.orrillo@moldeo.coop>
2016-09-06 14:58:15 -0300

    Made changes to report

--- __openerp__.py
+++ __openerp__.py
@@ -5,7 +5,8 @@
     'depends': ['elmatica_sales_customizations','elmatica_invoice','elmatica_purchase_flow','sale_crm','email_template','elmatica_wms','dhl_delivery_carrier'],
     'data': [
 	'sale_view.xml',
-	'declaration_of_conformity.xml'
+	'declaration_of_conformity.xml',
+	'po20_report.xml'
     ],
     'demo': [
     ],
